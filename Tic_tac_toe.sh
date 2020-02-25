d11fe15c4966b8a5a655233cfce509482565e1fb
Kartikey45 <singhkartikey45@gmail.com>
2020-02-25 10:33:10 +0530

    ADDED:player starts with resetting the board

--- Tic_tac_toe.sh
+++ Tic_tac_toe.sh
@@ -2,3 +2,24 @@
 
 echo "*****************Welcome to Tic Tac Toe****************"
 
+#INITIALIZE VARIABLES AND CONSTANTS
+ROW_LIMIT=3
+COLUMN_LIMIT=3
+
+#DECLARE 2-D DICTIONARY
+declare -A board
+
+#RESET THE BOARD
+function boardReset()
+{
+	for((row=0; row<$ROW_LIMIT; row++))
+	do
+		for((column=0; column<$COLUMN_LIMIT; column++))
+		do
+			board[$row,$column]=" "
+		done
+	done
+}
+
+#FUNCTION CALL
+boardReset
