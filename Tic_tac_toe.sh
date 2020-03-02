241d0fa310672f8de5328e030685b4dbcf3adf1c
Kartikey45 <singhkartikey45@gmail.com>
2020-03-02 18:47:56 +0530

    ADDED:Show Winner, Tie or change the turn

--- Tic_tac_toe.sh
+++ Tic_tac_toe.sh
@@ -6,8 +6,11 @@ echo "*****************Welcome to Tic Tac Toe****************"
 ROW_LIMIT=3
 COLUMN_LIMIT=3
 SIGN=0
+firstMove=1
+lastMove=9
+count=0
 
-#DECLARE 2-D DICTIONARY
+#DECLARE THE DICTIONARY
 declare -A board
 
 #RESET THE BOARD
@@ -45,13 +48,15 @@ function tossBegin()
 	if [ $tossCheck -eq $SIGN ]
 	then
 		echo "player wins the toss"
+		contestant=$player
 	else
 		echo "Computer wins the toss"
+		contestant=$computer
 	fi
 }
 
 #DISPLAY THE BOARD
-function board()
+function displayBoard()
 {
 	echo "Empty board:"
 	for((row=0; row<ROW_LIMIT; row++))
@@ -65,8 +70,148 @@ function board()
 	done
 }
 
+#PLAYER CHOOSE THE CELLS TO FILL THE BOARD
+function cellChooseByBothPlayers()
+{
+	for((turn=$firstMove; turn<=$lastMove; turn++))
+	do
+		read -p "User has to choose row to insert the value :" row
+		read -p "User has to choose column to insert the value :" column
+		board[$row,$column]=$player
+		cellChooseByComputer
+		displayBoard
+		row=0
+		column=0
+		if [[ ${board[$row,$column]}${board[$row,$(($column+1))]}${board[$row,$(($column+2))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$column]}${board[$row,$(($column+1))]}${board[$row,$(($column+2))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		elif [[ ${board[$(($row+1)),$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+1)),$(($column+2))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$(($row+1)),$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+1)),$(($column+2))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		elif [[ ${board[$(($row+2)),$column]}${board[$(($row+2)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$(($row+2)),$column]}${board[$(($row+2)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+      	echo "Computer Wins !!!"
+      	break
+		elif [[ ${board[$row,$column]}${board[$(($row+1)),$column]}${board[$(($row+2)),$column]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$column]}${board[$(($row+1)),$column]}${board[$(($row+2)),$column]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+1))]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+1))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+1))]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+1))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "computer Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+2))]}${board[$(($row+1)),$(($column+2))]}${board[$(($row+2)),$(($column+2))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+2))]}${board[$(($row+1)),$(($column+2))]}${board[$(($row+2)),$(($column+2))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		elif [[ ${board[$row,$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]} == $player$player$player ]]
+		then
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+2))]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$column]} == $player$player$player ]]
+		then 
+			(( count++ ))
+			echo "Player Wins !!!"
+			break
+		elif [[ ${board[$row,$(($column+2))]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$column]} == $computer$computer$computer ]]
+		then
+			(( count++ ))
+			echo "Computer Wins !!!"
+			break
+		fi
+	done
+	if [ $count -eq 0 ]
+	then
+		echo "Tie !!!"
+		break
+	fi
+}
+
+#COMPUTER CHOOSE THE CELLS TO FILL THE BOARD
+function cellChooseByComputer()
+{
+	echo "Computer has to choose the cell :"
+	random=$(( RANDOM%9+1 ))
+	case $random in 
+		1)
+		board[0,0]=$computer
+		;;
+		2)
+		board[0,1]=$computer
+		;;
+		3)
+		board[0,2]=$computer
+		;;
+		4)
+		board[1,0]=$computer
+		;;
+		5)
+		board[1,1]=$computer
+		;;
+		6)
+		board[1,2]=$computer
+		;;
+		7)
+		board[2,0]=$computer
+		;;
+		8)
+		board[2,1]=$computer
+		;;
+		9)
+		board[2,2]=$computer
+		;;
+		*)
+		echo "invalid"
+		;;
+	esac
+}
+
 #FUNCTIONS CALL
 boardReset
 letterAssigned
 tossBegin
-board
+cellChooseByBothPlayers
